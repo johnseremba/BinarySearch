4fb2df66f0ae57242166ccc9d8cb6cf99494cc50
SerryJohns <serembaj@gmail.com>
2017-04-20 17:22:40 +0300

    Solve list indexing

--- binarySearch.py
+++ binarySearch.py
@@ -1,36 +1,29 @@
 class BinarySearch(list):
     def __init__(self, a, b):
-        self.array = [x for x in range(b, a * b, b)]
+        self.array = [x for x in range(b, a * b + 1, b)]
+        super(BinarySearch, self).__init__(self.array)
         self.length = len(self.array)
+
+    def __getitem__(self, item):
+        return self.array[item]
 
     def search(self, param):
         count = 0
-        print(self.array)
-
         array = self.array
         lower = 0
         upper = len(array)
 
         while lower < upper:
-            try:
-                count += 1
-                mid = lower + (upper - lower) // 2
-                value = self.array[mid]
-                if param == value:
-                    return {"count": count, "index": mid}
-                elif param > value:
-                    if lower == mid:
-                        break
-                    lower = mid + 1
-                else:
-                    upper = mid - 1
-            except:
-                break
+            count += 1
+            mid = lower + (upper - lower) // 2
+            value = self.array[mid]
+
+            if param == value:
+                return {"count": count, "index": mid}
+            elif param > value:
+                if lower == mid:
+                    break
+                lower = mid + 1
+            else:
+                upper = mid - 1
         return {"count": count, "index": -1}
-        # print(self.array)
-
-mySearch = BinarySearch(100, 10)
-print(mySearch.search(1000))
-
-# elif param > self.array[upper - 1]:
-#                     return {"count": count, "index": -1}
