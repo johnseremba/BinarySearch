4a9519f49ba74157e9aed985ce0ff66cb65d934c
SerryJohns <serembaj@gmail.com>
2017-04-20 18:08:42 +0300

    Delete test.py

--- try.py
+++
@@ -1,9 +0,0 @@
-class WrappedList(list):
-    def __init__(self):
-        super(WrappedList, self).__init__(self.list)
-
-    def __getitem__(self, item):
-        return self.list[item]
-
-w = WrappedList()
-print(w[1])
